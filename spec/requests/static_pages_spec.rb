57cf9346dd9bac1ca4447a3a806754dbaa481c9d
yoshihiro <yoshiherod@gmail.com>
2014-05-14 15:11:32 +0000

    refactoring

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Static pages" do
+	
+	let(:base_title) {"Ruby on Rails Tutorial Sample App"}
 
 	describe "Home page" do
 	
@@ -11,7 +13,7 @@ describe "Static pages" do
 		
 		it "should have the right title" do
 			visit '/static_pages/home'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+			expect(page).to have_title("#{base_title} | Home")
 		end
 	end
 	
@@ -22,7 +24,7 @@ describe "Static pages" do
 		end
 		it "should have the right title" do
 			visit '/static_pages/help'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+			expect(page).to have_title("#{base_title} | Help")
 		end
 	
 	end
@@ -34,7 +36,18 @@ describe "Static pages" do
 		end
 		it "should have the right title" do
 			visit '/static_pages/about'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+			expect(page).to have_title("#{base_title} | About Us")
+		end
+	end
+	
+	describe "Contact page" do
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("#{base_title} | Contact")
 		end
 	end
 end
