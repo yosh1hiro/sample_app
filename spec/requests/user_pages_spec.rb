67abb24e20a557852548d31386f0e66c37314d38
yoshihiro <yoshiherod@gmail.com>
2014-05-28 04:38:29 +0000

    Finish user edit, update, index, and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,13 +4,62 @@ describe "User Pages" do
 	
 	subject { page }
 	
+	describe "index" do
+		let(:user) { FactoryGirl.create(:user) }
+		before do
+			sign_in user
+			FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+			FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+			visit users_path		
+		end
+		
+		it { should have_title('All users') }
+		it { should have_content('All users') }
+		
+		describe "pagination" do
+			
+			before(:all) { 30.times { FactoryGirl.create(:user) } }
+			after(:all) { User.delete_all }
+			
+			it { should have_selector('div.pagination') }
+		
+		
+			it "should list each user" do
+				User.paginate(page: 1).each do |user|
+					expect(page).to have_selector('li', text: user.name)
+				end
+			end
+		end
+		
+		describe "delete links" do
+			
+			it { should_not have_link('delete') }
+			
+			describe "as an admin user" do
+				let(:admin) { FactoryGirl.create(:admin) }
+				before do
+					sign_in admin
+					visit users_path
+				end
+				
+				it { should have_link('delete', href: user_path(User.first)) }
+				it "should be able to delete another user" do
+					expect do
+						click_link('delete', match: :first)
+					end.to change(User, :count).by(-1)
+				end
+				it { should_not have_link('delete', href: user_path(admin)) }
+			end
+		end
+	end
+	
 	describe "profile page" do
 		let(:user) { FactoryGirl.create(:user) }
 		before { visit user_path(user) }
 		
 		it { should have_content(user.name) }
 		it { should have_title(user.name) }
-	end 
+	end
 	
 	describe "signup page" do
 		before { visit signup_path }
@@ -59,6 +108,70 @@ describe "User Pages" do
 				
 			end
 		end
+	end
+	
+	describe "edit" do
+		let(:user) { FactoryGirl.create(:user) }
+		before do
+			sign_in user
+			visit edit_user_path(user)
+		end
 		
+		describe "page" do
+			it { should have_content("Update your profile") }
+			it { should have_title("Edit user") }
+			it { should have_link('change', href: 'http://gravatar.com/emails') }
+		end
+		
+		describe "with invalid information" do
+			before { click_button "Save changes" }
+			
+			it { should have_content('error') }
+		end
+		
+		describe "with valid information" do
+			let(:new_name) { "New name" }
+			let(:new_email) { "new@example.com" }
+			
+			before do
+				fill_in "Name", with: new_name
+				fill_in "Email", with: new_email
+				fill_in "Password", with: user.password
+				fill_in "Confirmation", with: user.password
+				click_button "Save changes"
+			end
+			
+			it { should have_title(new_name) }
+			it { should have_selector('div.alert.alert-success') }
+			it { should have_link('Sign out', href: signout_path) }
+			specify { expect(user.reload.name).to eq new_name }
+			specify { expect(user.reload.email).to eq new_email }
+		end
+		
+		describe "fobidden attributes" do
+			let(:params) do
+				{ user: { admin: true, password: user.password,
+						password_confirmation: user.password } }
+			end
+			before do
+				sign_in user, no_capybara: true
+				patch user_path(user), params
+			end
+			specify { expect(user.reload).not_to be_admin }
+		end
 	end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
